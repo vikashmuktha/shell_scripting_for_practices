aebb620711b8c80512491b989911ebd4723ece10
vikash <mukthavikash@gmail.com>
2023-04-25 19:02:01 +0530

    Shell Script for Color Examples

---
+++ colors.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+Bl="\e[30m" #---This for Black Colour---#
+
+R="\e[31m"  #---This for Red Colour---#
+
+G="\e[32m"  #---This for Green Colour---#
+
+Y="\e[33m"  #---This for Yellow Colour---#
+
+B="\e[34m"  #---This for Blue Colour---#
+
+N="\e[0m"  #---This for Normal Colour---#
+
+
+echo "Hello World"
+
+
+#-----Testing Black Color-------#
+
+echo -e "This test for ${Bl} Color ${N} Testing"
+
+
+#------Testing Red Color-------#
+
+echo -e "This test for ${R} Color ${N} Testing"
+
+
+
+#------Testing Green Color-------#
+
+echo -e "This test for ${G} Color ${N} Testing"
+
+
+#------Testing Yellow Color-------#
+
+echo -e "This test for ${Y} Color ${N} Testing"
+
+
+
+#------Testing Normal Color-------#
+
+echo -e "This test for ${N} Color ${N} Testing"
+
+
+#-----Testing Blue Color-------#
+
+echo -e "This test for ${B} Color ${N} Testing"
+
+
+
+
+
